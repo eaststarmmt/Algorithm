582f77007ffead706eb99eeaa1736870727f621a
DongKyu Kim <aass0903@naver.com>
2020-12-25 17:31:21 +0900

    2798 complete

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -1,22 +1,27 @@
 #include<iostream>
-#include<utility>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
-	pair <int, int> a[50];
+	int n, m;
+	cin >> n >> m;
+	vector <int> card;
 	for (int i = 0; i < n; i++) {
-		cin >> a[i].first >> a[i].second;
+		int x;
+		cin >> x;
+		card.push_back(x);
 	}
-	int rank = 1;
+	vector <int> result;
+	sort(card.begin(), card.end(), greater<int>());
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (a[i].first < a[j].first && a[i].second < a[j].second) {
-				rank++;
+		int sum = 0;
+		for (int j = i + 1; j < n - 1; j++) {
+			for (int k = j + 1; k < n; k++) {
+				sum = card[i] + card[j] + card[k];
+				if (sum <= m) result.push_back(sum);
 			}
 		}
-		cout << rank << ' ';
-		rank = 1;
 	}
+	cout << *max_element(result.begin(), result.end()) << endl;
 }
