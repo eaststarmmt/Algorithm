8c1f29d67054872db9af3a86152d1626d8c03118
DongKyu Kim <aass0903@naver.com>
2021-01-11 18:11:45 +0900

    Question 4949 complete

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -1,6 +1,48 @@
 #include<iostream>
+#include<stack>
+#include<string>
 using namespace std;
 
 int main() {
-
+	while (1) {
+		string n;
+		stack<char> stk;
+		bool flag = true;
+		getline(cin, n);
+		if (n == ".") {
+			break;
+		}
+		for (int i = 0; i < n.size(); i++) {
+			if (n[i] == '(') {
+				stk.push('(');
+			}
+			else if (n[i] == ')') {
+				if (stk.empty() || stk.top() != '(') {
+					cout << "no" << endl;
+					flag = false;
+					break;
+				}
+				else {
+					stk.pop();
+				}
+			}
+			else if (n[i] == '[') {
+				stk.push('[');
+			}
+			else if (n[i] == ']') {
+				if (stk.empty() || stk.top() != '[') {
+					cout << "no" << endl;
+					flag = false;
+					break;
+				}
+				else {
+					stk.pop();
+				}
+			}
+		}
+		if (flag) {
+			if (stk.empty()) cout << "yes" << endl;
+			else cout << "no" << endl;
+		}
+	}
 }
