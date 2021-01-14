8ccee1b43433e818a5ac68784772032a4f54099b
DongKyu Kim <aass0903@naver.com>
2021-01-14 22:33:54 +0900

    Question 1021 complete

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -11,33 +11,35 @@ int main() {
 	for (int i = 1; i < n + 1; i++) {
 		deque.push_back(i);
 	}
-	for (int i = 0; i < m; i++) {
-		int x;
-		cin >> x;
-		que.push(x);
-	}
 	int count = 0;
-	while (!que.empty()) {
-		int index;
-		for (int i = 0; i < deque.size() < i++;) {
-			deque[i] == que.front();
-			index = i;
+	for (int i = 0; i < m; i++) {
+		int index, x;
+		cin >> x;
+		for (int j = 0; j < deque.size(); j++) {
+			if (deque[j] == x) {
+				index = j;
+				break;
+			}
 		}
-		if (deque.front() == que.front()) {
-			deque.pop_front();
-			que.pop();
-		}
-		else {
-			count++;
-			if (index > deque.size() - index) {
-				deque.push_front(deque.back());
-				deque.pop_back();
+		while (1) {
+			if (deque.front() == x) {
+				deque.pop_front();
+				break;
 			}
 			else {
-				deque.push_back(deque.front());
-				deque.pop_front();
+				count++;
+				if (index > deque.size() - index) {
+					deque.push_front(deque.back());
+					deque.pop_back();
+				}
+				else {
+					deque.push_back(deque.front());
+					deque.pop_front();
+				}
 			}
+
 		}
+	
 	}
 	cout << count << endl;
 }
