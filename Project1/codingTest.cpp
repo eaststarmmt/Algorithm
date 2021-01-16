e0eed0b801d4e5408abfed3a6a2991e42e57a942
DongKyu Kim <aass0903@naver.com>
2021-01-16 22:27:55 +0900

    Question 11729 not yet

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+void hanoi(int n, int from, int by, int to) {
+	if (n == 1) cout << from << ' ' << to << "\n";
+	else {
+		hanoi(n - 1, from, to, by);
+		cout << from << ' ' << to << "\n";
+		hanoi(n - 1, by, from, to);
+	}
+}
 
+int main() {
+	int n;
+	cin >> n;
+	hanoi(n, 1, 2, 3);
 }
