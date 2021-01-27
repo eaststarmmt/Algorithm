4a6c369cd0c9b254ad321a61fd8e6c1f26291173
DongKyu Kim <aass0903@naver.com>
2021-01-27 17:22:07 +0900

    Question 2606 not yet

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
+#include<queue>
+#include<vector>
 using namespace std;
 
-int main() {
+bool visited[101];
+vector<int> graph[101];
 
+void bfs(int start) {
+	queue<int> que;
+	que.push(start);
+	visited[start] = true;
+	while (!que.empty()) {
+		
+	}
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	bfs(1);
 }
