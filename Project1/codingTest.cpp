32b6d63bfea8300c7604689fe258161c333d9daa
DongKyu Kim <aass0903@naver.com>
2021-02-11 19:22:01 +0900

    Question 2667 complete

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -1,6 +1,51 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stdio.h>
 using namespace std;
 
-int main() {
+int graph[25][25];
+int n;
+vector<int> sum;
+int cnt = 0;
+int complex = 1;
+bool visited[25][25];
 
+bool dfs(int x, int y) {
+	if (x < 0 || y < 0 || x >= n || y >= n) return false;
+	if (visited[x][y]) return false;
+	visited[x][y] = true;
+	if (graph[x][y] > 0 ) {
+		graph[x][y] = complex;
+		cnt++;
+		dfs(x - 1, y);
+		dfs(x + 1, y);
+		dfs(x, y - 1);
+		dfs(x, y + 1);
+		return true;
+	}
+	return false;
+}
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			scanf("%1d", &graph[i][j]);
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (dfs(i, j)) {
+				complex++;
+				sum.push_back(cnt);
+			}
+			cnt = 0;
+		}
+	}
+	sort(sum.begin(), sum.end());
+	cout << complex - 1 << "\n";
+	for (int i = 0; i < sum.size(); i++) {
+		cout << sum[i] << "\n";
+	}
 }
