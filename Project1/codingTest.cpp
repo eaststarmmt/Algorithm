985b4b0a11f71eaab17cf9e2f98c66feaeac6e69
DongKyu Kim <aass0903@naver.com>
2021-02-13 15:54:49 +0900

    Question 1012 complete

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -1,6 +1,44 @@
 #include<iostream>
 using namespace std;
+int graph[50][50];
+int m, n, k, cnt;
+
+bool dfs(int x, int y) {
+	if (x < 0 || y < 0 || x >= m || y >= n) return false;
+	if (graph[x][y] == 1) {
+		graph[x][y] = 0;
+		dfs(x - 1, y);
+		dfs(x, y - 1);
+		dfs(x + 1, y);
+		dfs(x, y + 1);
+		return true;
+	}
+	return false;
+}
 
 int main() {
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> m >> n >> k;
+		for (int j = 0; j < k; j++) {
+			int x, y;
+			cin >> x >> y;
+			graph[x][y] = 1;
+		}
+		for (int j = 0; j < m; j++) {
+			for (int k = 0; k < n; k++) {
+				if (dfs(j, k)) cnt++;
+			}
+		}
+		cout << cnt << "\n";
+		
+		if(i < t - 1) {
+			for (int j = 0; j < m; j++) {
+				for (int k = 0; k < n; k++) graph[j][k] = 0;
+			}
+			cnt = 0;
+		}
+	}
 
 }
