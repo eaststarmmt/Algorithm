d999a8f16f4238b66785468a71601811533c78a8
DongKyu Kim <aass0903@naver.com>
2021-03-01 19:49:25 +0900

    Question 10989 complete

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -1,6 +1,20 @@
-#include<iostream>
+#include<stdio.h>
+#include<algorithm>
 using namespace std;
 
-int main() {
+int num[10001];
 
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		int x;
+		scanf("%d", &x);
+		num[x]++;
+	}
+	for (int i = 1; i < 10001; i++) {
+		for (int j = 0; j < num[i]; j++) {
+			printf("%d\n", i);
+		}
+	}
 }
