1b3a89edd2df9126ee73855c3a78b04fc2ff77f3
DongKyu Kim <aass0903@naver.com>
2021-03-03 16:53:14 +0900

    Question 1427 complete

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -1,6 +1,21 @@
-#include<stdio.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
 int main() {
-
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	string input;
+	cin >> input;
+	vector<int> vec;
+	for (int i = 0; i < input.size(); i++) {
+		vec.push_back(input[i] - '0');
+	}
+	sort(vec.begin(), vec.end(), greater<int>());
+	for (int i = 0; i < vec.size(); i++) {
+		cout << vec[i];
+	}
+	cout << "\n";
 }
