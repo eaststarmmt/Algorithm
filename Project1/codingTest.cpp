38797ffc9a117e1a799abf34b141c8020038e57f
DongKyu <aass0903@naver.com>
2021-07-25 22:49:55 +0900

    modify comment

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -15,7 +15,7 @@ int main() {
 		if (input[i] == '(') stick.push('(');	// ( 나오면 무조건 push
 		else {									// ) 나오면 무조건 pop
 			stick.pop();
-			if (input[i - 1] == '(') {			// 바로 전이 ( 였으면 스틱 갯수 만큼 더해줌
+			if (input[i - 1] == '(') {			// 바로 전이 ( 였으면 스틱 개수 만큼 더해줌
 				sum += stick.size();
 			}
 			else {				// 잘린 부분은 더해줬으나 원래 있던 막대기는 더해주지 못했기 때문에
