649fd84f32a37b5bbd4bbb5fe38f854ca69827c6
DongKyu <aass0903@naver.com>
2021-08-20 23:05:42 +0900

    Question 22856 the second way fail

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -1,39 +1,60 @@
 #include<stdio.h>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
-bool node[5000002];
+//bool node[5000002];
+int node[100001][3];
+bool check[5000001];	// 단순 카운팅 용
 
 int main() {
 	// vector<int>[10000] 0부모 1오른쪽 2왼쪽
 	int N;
 	scanf("%d", &N);
 	int lastLeaf = 0;
+	
 	for (int i = 0; i < N; i++) {
 		int a, b, c;
 		scanf("%d %d %d", &a, &b, &c);
-		node[a] = true;
-		lastLeaf = max(lastLeaf, a);
+		if (b != -1)
+			node[a][1] = b;
+		if (c != -1)
+			node[a][2] = c;
+		if (b != -1)
+			node[b][0] = a;
+		if (c != -1)
+			node[c][0] = a;
 	}
 	int cnt = -1;
 	int now = 1;
+	check[now] = true;
+	int visit = 1;
 	while (true) {
 		cnt++;
-		if (now == lastLeaf) {
+		if (!check[now]) {		
+			check[now] = true;
+			visit++;
+		}
+		if (visit == N) {		// 방문한 곳이 N이 되면 마지막 방문을 했다는것으로 간주함
 			printf("%d", cnt);
 			return 0;
 		}
 		
-		if (node[now * 2]) {	// 왼쪽 자식 있는 경우
-			now = now * 2;
+		if (node[now][1] != 0) {	// 왼쪽 자식 있는 경우
+			int x = node[now][1];
+			node[now][1] = 0;
+			now = x;
 		}
-		else {					// 왼쪽 자식 없는 경우
-			if (node[now * 2 + 1]) {	// 오른쪽 있는지 확인
-				now = now * 2 + 1;
+		else {						// 왼쪽 자식 없는 경우
+			if (node[now][2] != 0) {	// 오른쪽 있는지 확인
+				int x = node[now][2];
+				node[now][2] = 0;
+				now = x;
 			}
 			else {		// 자식 둘다 없음
-				node[now] = false;	// 무한루프 막기 위해
-				now = now / 2;
+				int x = node[now][0];
+			
+				now = x;
 				
 			}
 		}
