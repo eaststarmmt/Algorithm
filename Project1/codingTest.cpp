77c978b5fe106eccb4666deed8e5182245f1c4c2
DongKyu Kim <aass0903@naver.com>
2021-09-26 17:48:29 +0900

    Question 2636 complete

---
+++ Project1/codingTest.cpp
@@ -0,0 +1,64 @@
+#include<cstdio>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+int map[100][100];
+bool visited[100][100];
+int dr[] = { -1, 1, 0, 0 };
+int dc[] = { 0, 0, -1, 1 };
+int R, C;
+
+void bfs() {
+	queue<pair<int, int>> Q;
+	vector<int> cheeze;
+	int time = 0;
+	while (true) {
+		Q = queue<pair<int, int>>();	// 큐 초기화
+		Q.push({ 0, 0 });	// 항상 0, 0 부터 시작
+		for (int i = 0; i < R; i++) {	// visited 초기화
+			fill(visited[i], visited[i] + C + 1, false);
+		}
+		visited[0][0] = true;
+		int cnt = 0;
+		while (!Q.empty()) {
+			int r = Q.front().first;
+			int c = Q.front().second;
+			Q.pop();
+			// 공기중에 닿은 치즈인 경우. 밖에서 시작하므로 인접한 위치를 push 안하면 공기중에 노출된 치즈만 만남
+			if (map[r][c] == 1) {
+				map[r][c] = 0;
+				cnt++;
+				continue;
+			}
+
+			for (int i = 0; i < 4; i++) {
+				int nr = r + dr[i];
+				int nc = c + dc[i];
+				// 치즈인 경우 이곳에 오지 않으므로 인접하면 일단 다 넣어도 됨
+				if (nr >= 0 && nr < R && nc >= 0 && nc < C && !visited[nr][nc]) {
+					Q.push({ nr, nc });
+					visited[nr][nc] = true;
+				}
+			}	// end of for 사방탐색
+		}	// end of while Q
+		if (cnt == 0) break;	// 치즈 없으면 무한루프 탈출
+		cheeze.push_back(cnt);
+		time++;
+	}	// end of while 무한루프
+
+	printf("%d\n", time);
+	printf("%d\n", cheeze[time - 1]);
+}
+
+int main() {
+
+	scanf("%d %d", &R, &C);
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			scanf("%d", &map[i][j]);
+		}
+	}
+	bfs();
+}
