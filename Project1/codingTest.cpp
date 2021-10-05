e42301396a8c74748a5739a863c8544c48f4ce26
DongKyu <aass0903@naver.com>
2021-10-05 21:26:50 +0900

    Question 17142 complete

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -1,122 +1,102 @@
+#include<cstdio>
 #include<iostream>
 #include<queue>
 #include<vector>
 #include<cstring>
-
-#define endl "\n"
-#define MAX 50
 using namespace std;
 
-int N, M, Empty_Place, Answer = 987654321;
-int MAP[MAX][MAX];
-int Time[MAX][MAX];
-bool Select[10];
+int map[51][51];
+int time[51][51];
+int dr[] = { -1, 1, 0, 0 };
+int dc[] = { 0, 0, -1, 1 };
+vector<pair<int, int>> virus;
+vector<pair<int, int>> send;
+bool selected[11];
+int N, M;
+int res = 1e9;
+int blank;
 
-vector<pair<int, int>> Virus;
+void bfs(queue<pair<int, int>> Q) {
+	int lastTime = 0;
+	int cnt = 0;
 
-int dx[] = { 0, 0, 1, -1 };
-int dy[] = { 1, -1, 0, 0 };
-
-int Min(int A, int B) { if (A < B) return A; return B; }
-
-void Input()
-{
-	cin >> N >> M;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			cin >> MAP[i][j];
-			if (MAP[i][j] == 0) Empty_Place++;
-			else if (MAP[i][j] == 2) Virus.push_back(make_pair(i, j));
-		}
-	}
-}
-
-void Spread_Virus(queue<pair<int, int>> Q)
-{
-	int Infect_Place = 0;
-	int Total_Time = 0;
-	while (Q.empty() == 0)
-	{
-		int x = Q.front().first;
-		int y = Q.front().second;
+	while (!Q.empty()) {
+		int r = Q.front().first;
+		int c = Q.front().second;
 		Q.pop();
 
-		for (int i = 0; i < 4; i++)
-		{
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+		if (cnt == blank) break;		// 바이러스가 빈칸만큼 전이 했으면 탈출
 
-			if (nx >= 0 && ny >= 0 && nx < N && ny < N)
-			{
-				if (MAP[nx][ny] != 1 && Time[nx][ny] == -1)
-				{
-					Time[nx][ny] = Time[x][y] + 1;
-					if (MAP[nx][ny] == 0)
-					{
-						Infect_Place++;
-						Total_Time = Time[nx][ny];
+		for (int i = 0; i < 4; i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+
+			if (nr >= 0 && nr < N && nc >= 0 && nc < N) {
+				if (map[nr][nc] != 1 && time[nr][nc] == -1) {
+					time[nr][nc] = time[r][c] + 1;
+					if (map[nr][nc] == 0) {				// 빈칸인 경우
+						lastTime = time[nr][nc];		// bfs 특성상 마지막에 올수록 시간 커짐
+						if (lastTime >= res) return;	// 기존 최소값보다 크면 탈출
+						cnt++;							// 빈칸을 전이 했으므로 카운팅
+
 					}
-					Q.push(make_pair(nx, ny));
+					Q.push({ nr, nc });
 				}
 			}
 		}
 	}
-	if (Infect_Place == Empty_Place)
-	{
-		Answer = Min(Answer, Total_Time);
-	}
+	if (cnt == blank)
+		res = lastTime < res ? lastTime : res;
+
 }
 
-void Select_Virus(int Idx, int Cnt)
-{
-	if (Cnt == M)
-	{
+void comb(int start, int cnt) {
+	if (cnt == M) {		// M개만큼 뽑으면
 		queue<pair<int, int>> Q;
-		memset(Time, -1, sizeof(Time));
-		for (int i = 0; i < Virus.size(); i++)
-		{
-			if (Select[i] == true)
-			{
-				Q.push(make_pair(Virus[i].first, Virus[i].second));
-				Time[Virus[i].first][Virus[i].second] = 0;
+		memset(time, -1, sizeof(time));	// 시간 전부 -1 로 초기화
+		for (int i = 0; i < virus.size(); i++) {
+			if (selected[i]) {
+				Q.push({ virus[i].first, virus[i].second });	// 큐에 값 넣기
+				time[virus[i].first][virus[i].second] = 0;		// 바이러스 있는 자리는 0으로 설정
 			}
 		}
-
-		Spread_Virus(Q);
+		bfs(Q);
 		return;
 	}
 
-	for (int i = Idx; i < Virus.size(); i++)
-	{
-		if (Select[i] == true) continue;
-		Select[i] = true;
-		Select_Virus(i + 1, Cnt + 1);
-		Select[i] = false;
+	for (int i = start; i < virus.size(); i++) {
+		if (selected[i]) continue;
+		selected[i] = true;
+		comb(i + 1, cnt + 1);
+		selected[i] = false;
+
 	}
+
 }
 
-void Solution()
-{
-	Select_Virus(0, 0);
-	if (Answer == 987654321) cout << -1 << endl;
-	else cout << Answer << endl;
-}
-
-void Solve()
-{
-	Input();
-	Solution();
-}
-
-int main(void)
-{
-	ios::sync_with_stdio(false);
+int main() {
+	//scanf("%d %d", &N, &M);
+	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+	cin >> N >> M;
 
-	Solve();
-
-	return 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 2)					// 바이러스 위치 따로 저장
+				virus.push_back({ i, j });
+			else if (map[i][j] == 0)			// 빈칸 개수 세기
+				blank++;
+		}
+	}
+	if (blank == 0) {		// 빈칸 개수 0이면 0 출력
+		printf("%d\n", 0);
+		return 0;
+	}
+	comb(0, 0);
+	if (res != 1e9)
+		cout << res << "\n";
+	else
+		cout << -1 << "\n";
 }
