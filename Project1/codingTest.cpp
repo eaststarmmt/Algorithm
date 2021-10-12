70fbe0ed0c850a86a892ba6c182585a54fdb35b4
DongKyu <aass0903@naver.com>
2021-10-12 22:22:58 +0900

    Question 2812 complete

--- Project1/codingTest.cpp
+++ Project1/codingTest.cpp
@@ -7,18 +7,17 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	//int product[] = { 1, 10, 100, 1000, 10000, 100000, 1000000, 1000000 };
 	int N, K;
-	string input;
+	string input;		// 숫자를 한자리씩 나누기 위해 string으로 받음
 	cin >> N >> K >> input;
 	stack<int> stk;
 	int cnt = K;
 
-	if (N == K) return 0;
+	if (N == K) return 0;		// ex 1자리에서 1개 지우면 출력할게 없음
 
 	for (int i = 0; i < N; i++) {
 		
-		while (K > 0 && !stk.empty() && stk.top() < input[i] -'0') {
+		while (K > 0 && !stk.empty() && stk.top() < input[i] -'0') {	// K가 0보다 크다는 조건이 없으면 필요 이상으로 지워짐. top보다 다음 숫자가 크면 스택에서 비움
 			stk.pop();
 			K--;
 		}
@@ -28,16 +27,13 @@ int main() {
 
 	vector<int> result;
 	int limit = stk.size();
-	/*for (int i = 0; i < limit; i++) {
-		result.push_back(stk.top());
-		stk.pop();
-	}*/
+
 	while (true) {
 		result.push_back(stk.top());
 		stk.pop();
 		if (stk.empty()) break;
 	}
-	for (int i = result.size() - 1; i >= 0; i--) {
+	for (int i = result.size() - 1; i >= 0 + K; i--) {		// K가 완전히 소거되지 않았을 경우를 대비해 +K로 잡음
 		cout << result[i];
 	}
 
