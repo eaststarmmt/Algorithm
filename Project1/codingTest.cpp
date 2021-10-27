d23c463d8877942df03251615679e71ab7868015
DongKyu <aass0903@naver.com>
2021-10-27 13:59:17 +0900

    commit for merge

---
+++ Project1/codingTest.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#define endl "\N"
+using namespace std;
+
+int num[9];
+bool checked[9];
+int N, K;
+
+void comb(int start) {
+
+	for(int i = start)
+}
+
+int main() {
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++) {
+		int x;
+		cin >> x;
+		num[i] = x;
+	}
+
+	comb(1);
+}
