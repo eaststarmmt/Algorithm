eb385d2d3c2c36f7d6dd46b0f0a5a09a710be786
DongKyu <aass0903@naver.com>
2022-07-28 22:23:33 +0900

    백준 16234 인구이동

--- BOJ/16234 인구 이동.cpp
+++ BOJ/16234 인구 이동.cpp
@@ -1,80 +1,73 @@
-#include<stdio.h>
-#include<queue>
-#include<vector>
-#include<cmath>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstring>
 using namespace std;
 
 int N, L, R;
-int map[50][50];		// 나라별 인구수 저장 배열
-int unions[50][50];		// 어느 연합에 들어가있는지 확인하기 위한 배열
-
+int map[54][54];
 int dr[] = { -1, 1, 0, 0 };
 int dc[] = { 0, 0, -1, 1 };
+bool visited[54][54];
 
-int moveCnt;			// 이동횟수 카운팅
+vector<pair<int, int>> pop_move;
 
-void move(int r, int c, int index) {
-	vector<pair<int, int>> unionPair;	// 같은 연합에 들어갈 나라 좌표 저장
-	unionPair.push_back({ r, c });
-	queue<pair<int, int>> queue;		// bfs에 사용할 큐
-	queue.push({ r, c });
-	unions[r][c] = index;
+void dfs(int r, int c) {
+	visited[r][c] = true;
+	pop_move.push_back({ r, c });
+	for (int i = 0; i < 4; i++) {
+		int nr = r + dr[i];
+		int nc = c + dc[i];
 
-	int cnt = 1;						// 연합에 포함된 나라 수
-	int sum = map[r][c];				// 연합 총 인구수
-
-	while (queue.size() > 0) {
-		int r = queue.front().first;
-		int c = queue.front().second;
-		queue.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-			if (0 <= nr && 0 <= nc && nr < N && nc < N && unions[nr][nc] == -1) {	// 좌표 범위에 맞고 아직 연합이 있는지 확인 안했으면 실행
-				int gap = abs(map[r][c] - map[nr][nc]);		// 차이가 L이상 R이하이면 큐에 집어넣고 좌표 저장하고 합과 카운팅 하기
-				if (L <= gap && gap <= R) {
-					queue.push({ nr, nc });
-					unionPair.push_back({ nr, nc });
-					unions[nr][nc] = index;
-					cnt++;
-					sum += map[nr][nc];
-				}
-			}
+		if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+		if (visited[nr][nc]) continue;
+		int dif = abs(map[nr][nc] - map[r][c]);
+		if (dif >= L && dif <= R) {
+			dfs(nr, nc);
 		}
-	}
-	for (int i = 0; i < unionPair.size(); i++) {	// 저장한 좌표에 있는 나라들 인구 이동
-		map[unionPair[i].first][unionPair[i].second] = sum / cnt;
-	}
+	}	// end of for
 }
 
 int main() {
-	scanf("%d %d %d", &N, &L, &R);
-	
-	for (int i = 0; i < N; i++) {	//input
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> L >> R;
+
+	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			scanf("%d", &map[i][j]);
+			cin >> map[i][j];
 		}
 	}
-
-	while (true) {
-		fill(&unions[0][0], &unions[N - 1][N], -1);	// 연합 번호 확인전에 -1로 초기화
-
-		int index = 0;		// 몇번 조합인지 구분하기 위한 변수
-
+	bool flag = true;
+	int res = 0;
+	while (flag) {
+		flag = false;
+		memset(visited, false, sizeof(visited));
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (unions[i][j] == -1) {
-					move(i, j, index);
-					index++;	// 연합 하나의 탐색이 끝났으므로 다음 연합 인덱스 값에 1을 추가
+				if (!visited[i][j]) {
+					pop_move.clear();
+					dfs(i, j);
+					if (pop_move.size() > 1) {
+						flag = true;
+					}
+					int sum = 0;
+					for (pair<int, int> x : pop_move) {
+						sum += map[x.first][x.second];
+					}	// end of for move
+					sum /= pop_move.size();
+					for (pair<int, int> x : pop_move) {
+						map[x.first][x.second] = sum;
+					}	// end of for move
 				}
-			}
-		}
+			}	// end of for i
+		}	// end of for j
+		if (flag) res++;
 
-		if (index == N * N)		// index가 N*N 이면 연합이 단 한개도 안나왔다는 뜻이므로 탈출
-			break;
-		moveCnt++;				// 연합이 있었으면 이동횟수 1 증가
-	}
+	}	// end of while
 
-	printf("%d\n", moveCnt);
+	cout << res << "\n";
+	return 0;
 }
