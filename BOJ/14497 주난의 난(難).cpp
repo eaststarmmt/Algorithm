d8a679363a1a11d1e67542476a912d068fa6f220
DongKyu Kim <aass0903@naver.com>
2022-08-05 20:14:45 +0900

    백준 14497 주난의 난(難) 큐 2개로 시간 단축

--- BOJ/14497 주난의 난(難).cpp
+++ BOJ/14497 주난의 난(難).cpp
@@ -13,44 +13,41 @@ int r1, c1, r2, c2;
 int res, cnt;
 
 void bfs() {
-	memset(visited, false, sizeof(visited));
 	queue<pair<int, int>> Q;
 	Q.push({ r1, c1 });
 	visited[r1][c1] = true;
-	cnt++;
-	vector<pair<int, int>> fri;
+	while (true) {
+		cnt++;
+		queue<pair<int, int>> temp;
+		while(!Q.empty()) {
+			int r = Q.front().first;
+			int c = Q.front().second;
+			Q.pop();
 
-	while (!Q.empty()) {
-		int r = Q.front().first;
-		int c = Q.front().second;
-		Q.pop();
-
-		if (r == r2 && c == c2) {
-			res = cnt;
-			break;
-		}
-
-		for (int i = 0; i < 4; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-
-			if (nr < 1 || nr > R || nc < 1 || nc > C || visited[nr][nc]) {
-				continue;
+			if (r == r2 && c == c2) {
+				res = cnt;
+				return;
 			}
 
-			if (map[nr][nc] == '1') {
-				fri.push_back({ nr, nc });
-			}
-			else {
-				Q.push({ nr, nc });
-			}
-			visited[nr][nc] = true;
+			for (int i = 0; i < 4; i++) {
+				int nr = r + dr[i];
+				int nc = c + dc[i];
+
+				if (nr < 1 || nr > R || nc < 1 || nc > C || visited[nr][nc]) {
+					continue;
+				}
+
+				if (map[nr][nc] == '1') {
+					temp.push({ nr, nc });
+				}
+				else {
+					Q.push({ nr, nc });
+				}
+				visited[nr][nc] = true;
+			}	// end of while Q
 		}	// end of for
+		Q = temp;
 	}	// end of while
-
-	for (pair<int, int> pos : fri) {
-		map[pos.first][pos.second] = '0';
-	}
 }
 
 int main() {
@@ -62,10 +59,8 @@ int main() {
 			scanf(" %1c", &map[i][j]);
 		}	// end of for j
 	}	// end of for i
-
-	while (!res) {
-		bfs();
-	}
+	
+	bfs();
 
 	printf("%d\n", res);
 	return 0;
