f2dfc5e50a9d3c9e4c4b3c8f7717df340eb92408
DongKyu Kim <aass0903@naver.com>
2022-11-28 20:19:16 +0900

    프로그래머스 행렬의 덧셈

---
+++ Programmers/행렬의 덧셈.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+	for (int i = 0; i < arr1.size(); i++) {
+		for (int j = 0; j < arr1[0].size(); j++) {
+			arr1[i][j] += arr2[i][j];
+		}   // end of for i
+	}   // end of for i
+	return arr1;
+}
