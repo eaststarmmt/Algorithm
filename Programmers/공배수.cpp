bb0ca51dc37fa3b12ee92aad844bf290d7e7ab37
DongKyu <aass0903@naver.com>
2023-09-09 00:07:10 +0900

    공배수

---
+++ Programmers/공배수.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int n, int m) {
+	int answer = number % n == 0 && number % m == 0 ? 1 : 0;
+	return answer;
+}
