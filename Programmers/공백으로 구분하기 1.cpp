67b08119ad44e170d86f1896f9286f12645417e3
DongKyu <aass0903@naver.com>
2023-09-17 02:17:01 +0900

    공백으로 구분하기 1

---
+++ Programmers/공백으로 구분하기 1.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+vector<string> solution(string my_string) {
+	vector<string> answer;
+	stringstream stream(my_string);
+	string s;
+	while (stream >> s) {
+		answer.push_back(s);
+	}
+	return answer;
+}
