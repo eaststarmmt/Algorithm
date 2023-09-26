f1b19c039a5087b3936506a86c90746179afe131
DongKyu <aass0903@naver.com>
2023-09-27 00:41:59 +0900

    문자열의 뒤의 n글자

---
+++ Programmers/문자열의 뒤의 n글자.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+	string answer = my_string.substr(my_string.size() - n);
+	return answer;
+}
