c6a687226008b6f55cf22c4558c63358f6fe6d6f
DongKyu <aass0903@naver.com>
2023-10-03 00:51:52 +0900

    n 번째 원소까지

---
+++ Programmers/n 번째 원소까지.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+	vector<int> answer(num_list.begin(), num_list.begin() + n);
+	return answer;
+}
