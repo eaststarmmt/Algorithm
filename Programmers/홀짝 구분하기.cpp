334967bf7d35c2ede1d8f2faca0290d562ad756e
DongKyu <aass0903@naver.com>
2023-11-17 02:18:50 +0900

    홀짝 구분하기

---
+++ Programmers/홀짝 구분하기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+	if (n % 2) cout << n << " is odd" << "\n";
+	else cout << n << " is even" << "\n";
+	return 0;
+}
