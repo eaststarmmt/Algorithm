ce4185456063eff9ee8774d9199940bc162cb41e
DongKyu <aass0903@naver.com>
2023-12-16 01:44:28 +0900

    접미사 배열

---
+++ Programmers/접미사 배열.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<string> solution(string my_string) {
+	vector<string> answer;
+	for (int i = 0; i < my_string.size(); i++) {
+		answer.push_back(my_string.substr(i));
+	}
+	sort(answer.begin(), answer.end());
+	return answer;
+}
