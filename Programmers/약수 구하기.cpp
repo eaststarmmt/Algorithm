6b514efca88f9e9acc2e0199e9a921b50b2308b6
DongKyu <aass0903@naver.com>
2023-12-23 18:07:00 +0900

    약수 구하기

---
+++ Programmers/약수 구하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+	vector<int> answer;
+	for (int i = 1; i <= n; i++) {
+		if (n % i) continue;
+		answer.push_back(i);
+	}
+	return answer;
+}
