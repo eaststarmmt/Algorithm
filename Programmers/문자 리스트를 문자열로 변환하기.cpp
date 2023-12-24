e336a1ddcba49f06ae7b3442149ca7abf7de6746
DongKyu <aass0903@naver.com>
2023-12-25 00:06:35 +0900

    문자 리스트를 문자열로 변환하기

---
+++ Programmers/문자 리스트를 문자열로 변환하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> arr) {
+	string answer = "";
+	for (string c : arr) {
+		answer += c;
+	}
+	return answer;
+}
