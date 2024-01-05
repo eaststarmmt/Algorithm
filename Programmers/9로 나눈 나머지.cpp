ab1d423dfb2a533449aa6bb7330718ffd52755fe
DongKyu <aass0903@naver.com>
2024-01-05 15:03:02 +0900

    9로 나눈 나머지

---
+++ Programmers/9로 나눈 나머지.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string number) {
+	int answer = 0;
+	for (char c : number) {
+		answer += c - '0';
+	}
+	return answer % 9;
+}
