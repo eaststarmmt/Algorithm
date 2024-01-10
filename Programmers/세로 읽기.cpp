d3f569b27acc4424f5507776e8d0fa4e58dde5fb
DongKyu <aass0903@naver.com>
2024-01-10 21:50:42 +0900

    세로 읽기

---
+++ Programmers/세로 읽기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int m, int c) {
+	string answer = "";
+	for (int i = c - 1; i < my_string.size(); i += m) {
+		answer += my_string[i];
+	}
+	return answer;
+}
