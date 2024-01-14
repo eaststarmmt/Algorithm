20e04698955ca28f8c78ca3687de9d4e88ef8e75
DongKyu <aass0903@naver.com>
2024-01-15 01:47:38 +0900

    문자열 섞기

---
+++ Programmers/문자열 섞기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string str1, string str2) {
+	string answer = "";
+	for (int i = 0; i < str1.size(); i++) {
+		answer += str1[i];
+		answer += str2[i];
+	}
+	return answer;
+}
