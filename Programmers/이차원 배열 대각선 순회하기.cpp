9d094613a7dbf504df86ab97f7b5236f660a7369
DongKyu <aass0903@naver.com>
2024-01-16 07:22:05 +0900

    이차원 배열 대각선 순회하기

---
+++ Programmers/이차원 배열 대각선 순회하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, int k) {
+    int answer = 0;
+    for(int i = 0; i < board.size(); i++) {
+        for(int j = 0; j < board[i].size(); j++) {
+            if(i + j > k) break;
+            answer += board[i][j];
+        }
+    }
+    return answer;
+}
