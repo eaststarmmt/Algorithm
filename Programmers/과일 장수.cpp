2731cd73808b584f240f599352e5a3abdd2c812a
DongKyu <aass0903@naver.com>
2024-04-21 23:44:18 +0900

    과일 장수

---
+++ Programmers/과일 장수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+	int answer = 0;
+	sort(score.begin(), score.end(), greater<int>());
+
+	for (int i = m - 1; i < score.size(); i += m) {
+		answer += score[i] * m;
+	}
+	return answer;
+}
